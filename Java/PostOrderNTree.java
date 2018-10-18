40f257677a8f65d8f4fafe7131761f49363a10b8
Kevin Nguyen <25444103+kevin-compsci@users.noreply.github.com>
2018-10-17 23:26:42 -0500

    Update PostOrderNTree.java

--- Java/PostOrderNTree.java
+++ Java/PostOrderNTree.java
@@ -1,5 +1,26 @@
-//postorder iterative traversal
-class Solution {
+/*
+Kevin Nguyen
+Basic Shell for Java programs.
+
+How to run (linux):
+	1.) open command line and cd into directory folder of where this code is
+	2.) type in "javac *.java" to compile java files
+	3.) type in "java JavaShell"
+*/
+
+//imports
+import java.io.*;
+import java.util.*;
+
+//class
+class PostOrderNTree {
+    //main driver
+	public static void main(String[] args) {
+		//local Declarations
+		System.out.println("HelloWorld");
+	}
+    
+    //postorder iterative traversal
     public List<Integer> postorder(Node root) {
         List<Integer> result = new ArrayList<Integer>();
         Stack<Node> myStack = new Stack<Node>();
