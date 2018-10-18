67399f3cd63e2bfa1cb84690c1228de83d86d1f2
Kevin Nguyen <25444103+kevin-compsci@users.noreply.github.com>
2018-10-17 23:31:32 -0500

    Update PostOrderNTree.java

--- Java/PostOrderNTree.java
+++ Java/PostOrderNTree.java
@@ -1,11 +1,11 @@
 /*
 Kevin Nguyen
-Basic Shell for Java programs.
+Basic Java program that takes in a tree and outputs an iterative post-order-traversal.
 
 How to run (linux):
 	1.) open command line and cd into directory folder of where this code is
 	2.) type in "javac *.java" to compile java files
-	3.) type in "java JavaShell"
+	3.) type in "java PostOrderNTree"
 */
 
 //imports
@@ -17,11 +17,11 @@ class PostOrderNTree {
     //main driver
 	public static void main(String[] args) {
 		//local Declarations
-		System.out.println("HelloWorld");
 	}
     
     //postorder iterative traversal
     public List<Integer> postorder(Node root) {
+	//local declarations
         List<Integer> result = new ArrayList<Integer>();
         Stack<Node> myStack = new Stack<Node>();
         myStack.push(root);
